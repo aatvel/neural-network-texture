a3980543e99dddbbd3f2f9f0461bea8b066e716b
Alina <alina.andrevna3993@gmail.com>
2023-01-07 17:29:29 +0300

    added bloom

--- src/Scene.js
+++ src/Scene.js
@@ -2,12 +2,14 @@ import { Center, OrbitControls, Environment } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import { Perf } from "r3f-perf";
-
+import React, { useRef } from "react";
 import Junior from "./Junior";
 import { Liquid } from "./Liquid";
 import Floor from "./Floor";
 
+
 export default function Scene() {
+
   return (
     <>
       <Canvas
@@ -20,10 +22,12 @@ export default function Scene() {
       >
         <OrbitControls makeDefault />
         <Environment preset="forest" />
-        <directionalLight position={[1.9, 0, - 1.3]} color={'magenta'}/>
-        <directionalLight position={[-2.9, 0, - 2]} color={'orange'}/>
-        <directionalLight position={[0, 0, 2]} color={'#fffaf0'} intensity={0.3} />
-        <directionalLight position={[0, 2, 1.2]} color={'#fffaf0'} intensity={0.2} />
+
+    
+        <directionalLight position={[1.9, 0, - 1.3]} color={'magenta'} />
+        <directionalLight position={[-2.9, 0, - 2]} color={'orange'}  />
+        {/* <directionalLight position={[0, 0, 2]} color={'#fffff0'} intensity={0.3} />
+        <directionalLight position={[0, 2, 1.2]} color={'#fffaf0'} intensity={0.3} /> */}
         <Center position={[0, 1.5, 0]}>
           <Suspense>
             <Junior scale={0.55} rotation={[0, Math.PI / 6, 0]} />
