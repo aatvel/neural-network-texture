70d7b32755c84dcabe94788a0da3bf590d694569
Alina <alina.andrevna3993@gmail.com>
2023-01-10 17:15:21 +0300

    changed model

--- src/Scene.js
+++ src/Scene.js
@@ -1,12 +1,14 @@
+import * as THREE from "three";
 import { Center, OrbitControls, Environment } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { Suspense } from "react";
 import { Perf } from "r3f-perf";
-import React, { useRef } from "react";
-import Junior from "./Junior";
-import { Liquid } from "./Liquid";
-import Floor from "./Floor";
+import React, { Suspense } from "react";
 
+import Floor from "./Floor";
+import { Model } from "./Model";
+import { Head } from "./Head";
+
+THREE.ColorManagement.legacyMode = false
 
 export default function Scene() {
 
@@ -17,7 +19,7 @@ export default function Scene() {
           fov: 45,
           near: 0.1,
           far: 200,
-          position: [0, 4, 20],
+          position: [0, 12, 20],
         }}
       >
         <OrbitControls makeDefault />
@@ -30,8 +32,8 @@ export default function Scene() {
         <directionalLight position={[0, 2, 1.2]} color={'#fffaf0'} intensity={0.3} />
         <Center position={[0, 1.5, 0]}>
           <Suspense>
-            <Junior scale={0.55} rotation={[0, Math.PI / 6, 0]} />
-            <Liquid scale={0.55} rotation={[0, Math.PI / 6, 0]} />
+            <Model scale={0.65} />
+            <Head scale={0.65} />
             <Floor />
           </Suspense>
         </Center>
